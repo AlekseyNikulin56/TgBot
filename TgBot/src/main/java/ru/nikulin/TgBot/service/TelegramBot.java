569c85422ce532438c556ad6c327b97b6d4f63f0
AlekseyNikulin56 <nikulin_a.d@mail.ru>
2023-10-09 15:09:08 +0400

    импорт класса BotConfig и создание конструктора TelegramBot

--- TgBot/src/main/java/ru/nikulin/TgBot/service/TelegramBot.java
+++ TgBot/src/main/java/ru/nikulin/TgBot/service/TelegramBot.java
@@ -3,9 +3,16 @@ package ru.nikulin.TgBot.service;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.nikulin.TgBot.config.BotConfig;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config){
+        this.config = config;
+    }
 
     @Override
     public String getBotUsername() {
