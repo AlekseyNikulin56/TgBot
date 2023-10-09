32742dd099660f844110b7eb8e78ccb79095d8fc
AlekseyNikulin56 <nikulin_a.d@mail.ru>
2023-10-09 15:30:25 +0400

    добавлена логика обработки сообщений в метод onUpdateReceived добавлен метод startCommandReceived

--- TgBot/src/main/java/ru/nikulin/TgBot/service/TelegramBot.java
+++ TgBot/src/main/java/ru/nikulin/TgBot/service/TelegramBot.java
@@ -16,17 +16,30 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return null;
+        return config.getBotName();
 
     }
 
     @Override
     public String getBotToken(){
-        return null;
+        return config.getToken();
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
+    public void onUpdateReceived(Update update) { //Главный метод для обработки сообщений
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText){
+                case "/start": startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+            }
+        }
+    }
+    private void startCommandReceived(long chadId, String name){
+
+        String answer = "Привет, " + name + ", рад видеть тебя!";
 
     }
+
 }
